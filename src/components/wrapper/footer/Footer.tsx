760b7f9ab160092b431a965c17b521ca19459e9f
NowyTeam <jaj2232ajn@gmail.com>
2023-07-14 15:57:01 +0600

    Really So Ez XD

--- src/components/wrapper/footer/Footer.tsx
+++ src/components/wrapper/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import scss from './Footer.module.scss'
 
 export const Footer = () => {
   return (
--- src/components/wrapper/header/Header.tsx
+++ src/components/wrapper/header/Header.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import scss from './Header.module.scss'
+
 
 export const Header = () => {
   return (
