5ab7f4a88f1e0236c43a40031b8f0a0db40884d0
NowyTeam <jaj2232ajn@gmail.com>
2023-07-14 16:25:31 +0600

    HeaderTS

--- src/components/wrapper/header/Header.tsx
+++ src/components/wrapper/header/Header.tsx
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from "react";
-import scss from './Header.module.scss'
-
+import { NavLink } from "react-router-dom";
+import scss from "./Header.module.scss";
 
 export const Header = () => {
+  const activeLink = (isActive) => {
+    return {
+      border: isActive ? "solid 1px white" : "none",
+    };
+  };
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <nav>
+      <div className={scss.header}>
+        <NavLink
+          to="/"
+          className={`${scss.link} ${scss.activeLinks}`}
+          style={activeLink(true)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/blog"
+          className={scss.link}
+          style={activeLink(false)}
+        >
+          Blog
+        </NavLink>
+      </div>
+    </nav>
   );
 };
