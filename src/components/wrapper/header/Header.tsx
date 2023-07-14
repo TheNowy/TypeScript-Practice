abd27801e65b125a987c508c433b7f047ba40609
NowyTeam <jaj2232ajn@gmail.com>
2023-07-14 16:26:42 +0600

    Fix

--- src/components/wrapper/header/Header.tsx
+++ src/components/wrapper/header/Header.tsx
@@ -3,29 +3,11 @@ import { NavLink } from "react-router-dom";
 import scss from "./Header.module.scss";
 
 export const Header = () => {
-  const activeLink = (isActive) => {
-    return {
-      border: isActive ? "solid 1px white" : "none",
-    };
-  };
-
   return (
     <nav>
       <div className={scss.header}>
-        <NavLink
-          to="/"
-          className={`${scss.link} ${scss.activeLinks}`}
-          style={activeLink(true)}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/blog"
-          className={scss.link}
-          style={activeLink(false)}
-        >
-          Blog
-        </NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
       </div>
     </nav>
   );
