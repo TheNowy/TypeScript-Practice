3dda0e98fe98abfec23b3ce56b58441e0f4de2e9
NowyTeam <jaj2232ajn@gmail.com>
2023-07-18 11:50:07 +0600

    FixBugs

--- src/components/wrapper/main/Main.tsx
+++ src/components/wrapper/main/Main.tsx
@@ -1,13 +1,13 @@
 import Home from "../../pages/home/Home";
 import Blog from '../../pages/blog/Blog'
-import {BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export const Main = () => {
   return (
     <div>
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog" element={<Blog />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </div>
   );
