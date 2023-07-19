e946598cfe9a7101f17f192e76062267e99e9b3b
NowyTeam <jaj2232ajn@gmail.com>
2023-07-19 13:13:26 +0600

    BurgerAnimation

--- src/components/wrapper/header/Header.tsx
+++ src/components/wrapper/header/Header.tsx
@@ -1,21 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import scss from "./Header.module.scss";
 
 export const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav>
       <div className={scss.header}>
         <div className={scss.header_wrapper}>
-          <div className={scss.logo}>
-            <h1>Nowy</h1>
-          </div>
+          <NavLink to="/" className={scss.logo}>
+            Nowy
+          </NavLink>
           <div className={scss.links}>
-            <NavLink to="/" className={scss.link}>
+            <div
+              className={`${scss.burger} ${isOpen ? scss.open : ""}`}
+              onClick={toggleMenu}
+            >
+              <span></span>
+              <span></span>
+            </div>
+            {/* <NavLink to="/" className={scss.link}>
               Home
             </NavLink>
             <NavLink to="/Catalog" className={scss.link}>
               Catalog
-            </NavLink>
+            </NavLink> */}
           </div>
         </div>
       </div>
