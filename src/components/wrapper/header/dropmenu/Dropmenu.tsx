d03fd9defd65fb38d0bcafd8bf33f9755c20721f
NowyTeam <jaj2232ajn@gmail.com>
2023-07-23 22:02:17 +0600

    DropDownAdpt

--- src/components/wrapper/header/dropmenu/Dropmenu.tsx
+++ src/components/wrapper/header/dropmenu/Dropmenu.tsx
@@ -3,16 +3,27 @@ import { NavLink } from "react-router-dom";
 import scss from "./Dropmenu.module.scss";
 
 const Dropmenu: React.FC = () => {
+  const activeLink = ({ isActive }: { isActive: boolean }) => {
+    return {
+      background: isActive ? "#ffffffa0" : "rgb(49, 49, 49)",
+      color: isActive ? "black" : "white",
+    };
+  };
+
   return (
-    <div className={scss.wrapper}>
-      <div className={scss.wrapper_box}>
-        <NavLink to="/" className={scss.link}>
-          Home
-        </NavLink>
-        <hr />
-        <NavLink to="/Catalog" className={scss.link}>
-          Catalog
-        </NavLink>
+    <div className={scss.wrapper_adpt}>
+      <div className={scss.wrapper_adpt_hlp}>
+        <div className={scss.wrapper}>
+          <div className={scss.wrapper_box}>
+            <NavLink to="/" className={scss.link} style={activeLink}>
+              Главная
+            </NavLink>
+            <hr />
+            <NavLink to="/Catalog" className={scss.link} style={activeLink}>
+              Каталог
+            </NavLink>
+          </div>
+        </div>
       </div>
     </div>
   );
