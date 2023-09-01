3a1050bd21d8b29d74eb35eadb0ab0a63e99343f
NowyTeam <jaj2232ajn@gmail.com>
2023-09-01 12:59:46 +0600

    TestMaterialUI

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import scss from "./Home.module.scss";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Home = () => {
   return (
@@ -6,7 +7,7 @@ const Home = () => {
       <div className={scss.page_wrapper}>
         <div className={scss.container}>
           <div className={scss.home_content}>
-            
+          <h1><GitHubIcon></GitHubIcon></h1>
           </div>
         </div>
       </div>
