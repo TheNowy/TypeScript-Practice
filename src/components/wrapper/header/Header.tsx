5b938b1cacdde72a6b0d160314bfe8d123505c7c
NowyTeam <jaj2232ajn@gmail.com>
2023-09-15 01:31:57 +0600

    UpdateNova

--- src/components/wrapper/header/Header.tsx
+++ src/components/wrapper/header/Header.tsx
@@ -1,20 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Dropmenu from "./dropmenu/Dropmenu";
 import scss from "./Header.module.scss";
 
+import DownloadIcon from "@mui/icons-material/Download";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import LoginIcon from "@mui/icons-material/Login";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import LaptopChromebookIcon from "@mui/icons-material/LaptopChromebook";
+
 export const Header: React.FC = () => {
+  const [headerScroll, setHeaderScroll] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [openProfile, setOpenProfile] = useState(false);
 
-  
+  useEffect(() => {
+    const changeBackground = () => {
+      if (typeof window !== "undefined" && window.scrollY >= 10) {
+        setHeaderScroll(true);
+      } else {
+        setHeaderScroll(false);
+      }
+    };
+
+    changeBackground();
+    -window.addEventListener("scroll", changeBackground);
+
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
+
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
     <nav>
-      <div className={scss.header}>
+      <div
+        className={
+          headerScroll
+            ? `${scss.header} ${scss.active}`
+            : `${scss.header}`
+        }>
         <div className={scss.header_wrapper}>
           <NavLink to="/" className={scss.logo}>
             <img src="../../../../../Logo&White.png" alt="" />
@@ -23,25 +52,31 @@ export const Header: React.FC = () => {
           </NavLink>
           <div className={scss.links}>
             <NavLink to="/" className={scss.linksD}>
-                Home
+              <SupportAgentIcon /> Поддержка
             </NavLink>
-            <NavLink to="/Catalog" className={scss.linksD}>
-                Catalog
+            <NavLink to="/" className={scss.linksD}>
+              <DownloadIcon /> Скачать
+            </NavLink>
+            <NavLink to="/" className={scss.linksD}>
+              <LocalMallIcon /> Купить
+            </NavLink>
+            <NavLink to="/" className={scss.cabinet}>
+              <LoginIcon />
+              Авторизация
             </NavLink>
             <div
               className={`${scss.burger} ${isOpen ? scss.open : ""}`}
               onClick={() => {
                 setOpenProfile((prev) => !prev);
                 toggleMenu();
-              }}
-            >
+              }}>
               <span></span>
               <span></span>
             </div>
           </div>
         </div>
       </div>
-      {openProfile && <Dropmenu/>}
+      {openProfile && <Dropmenu />}
     </nav>
   );
 };
